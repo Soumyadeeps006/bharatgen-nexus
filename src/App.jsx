c37bcf2e9b42d1fd88815d298f12b957f15e14b4
Soumyadeep Saha <Soumyadeeps006@gmail.com>
2026-06-25 17:44:30 +0530

    Modified the app added ontology

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
+import Pipelines from './pages/Pipelines';
+import Integrations from './pages/Integrations';
+import AnalyticsApps from './pages/AnalyticsApps';
+import Settings from './pages/Settings';
 
 function App() {
+  const [activePage, setActivePage] = useState('ontology');
+
+  const renderPage = () => {
+    switch (activePage) {
+      case 'ontology':
+        return <Dashboard />;
+      case 'pipelines':
+        return <Pipelines />;
+      case 'integrations':
+        return <Integrations />;
+      case 'apps':
+        return <AnalyticsApps />;
+      case 'settings':
+        return <Settings />;
+      default:
+        return <Dashboard />;
+    }
+  };
+
   return (
-    <Layout>
-      <Dashboard />
+    <Layout activePage={activePage} setActivePage={setActivePage}>
+      {renderPage()}
     </Layout>
   );
 }
